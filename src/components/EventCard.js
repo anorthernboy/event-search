aaae6afe2b479b01eccabc99781b5dda6cb201ff
craigweighman <craigweighman@gmail.com>
2019-04-28 11:03:07 +0100

    EventCard button CSS updated - same colour and onHover colour

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -13,41 +13,42 @@ const EventCard = ({
   date,
   eventType
 }) => {
-  console.log(eventType);
   return (
     <div className="EventCard">
-      <div className="event-grid">
-        <img src={eventImage} alt="" className="event-image" />
-        <div className="event-name">
-          {eventName.length <= 30
-            ? eventName
-            : eventName.slice(0, 30).trimEnd() + "..."}
+      <Link to={`/event/${eventId}`}>
+        <div className="event-grid">
+          <img src={eventImage} alt="" className="event-image" />
+          <div className="event-name">
+            {eventName.length <= 30
+              ? eventName
+              : eventName.slice(0, 30).trimEnd() + "..."}
+          </div>
+          <img
+            src={pin}
+            alt="location"
+            className="eventcard-icon location-icon"
+          />
+          <div className="eventcard-details event-location">{location}</div>
+          <img src={calendar} alt="date" className="eventcard-icon date-icon" />
+          <div className="eventcard-details event-date">
+            {new Date(date).toLocaleDateString("en-GB", {
+              weekday: "long",
+              day: "numeric",
+              month: "long",
+              year: "numeric"
+            })}
+          </div>
+          <Link to={`/event/${eventId}`} className="more-button view-details">
+            <div className="button-text">view details</div>
+          </Link>
+          <Link
+            to={`/events/${eventType}%20${location}`}
+            className="more-button view-similar"
+          >
+            <div className="button-text">more like this</div>
+          </Link>
         </div>
-        <img
-          src={pin}
-          alt="location"
-          className="eventcard-icon location-icon"
-        />
-        <div className="eventcard-details event-location">{location}</div>
-        <img src={calendar} alt="date" className="eventcard-icon date-icon" />
-        <div className="eventcard-details event-date">
-          {new Date(date).toLocaleDateString("en-GB", {
-            weekday: "long",
-            day: "numeric",
-            month: "long",
-            year: "numeric"
-          })}
-        </div>
-        <Link to={`/event/${eventId}`} className="more-button view-details">
-          <div className="button-text">view details</div>
-        </Link>
-        <Link
-          to={`/events/${eventType}%20${location}`}
-          className="more-button view-similar"
-        >
-          <div className="button-text">more like this</div>
-        </Link>
-      </div>
+      </Link>
     </div>
   );
 };
